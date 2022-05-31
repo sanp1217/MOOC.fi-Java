ec9591650edfa0d3f4a68d148d484a936d80b11c
Pablo <pablosgrad12@gmail.com>
2022-05-31 09:17:59 -0600

    Adds methods for ingredients.

--- part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
+++ part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 public class Recipe {
     private String name;
     private int cookTime;
-    
+    private ArrayList<String> ingredients;
     
     public Recipe(String name, int cookTime){
         this.name = name;
@@ -22,5 +24,13 @@ public class Recipe {
     
     public void setCookTime(int time){
         this.cookTime = time;
+    }
+    
+    public String getIngredient(int index){
+        return this.ingredients.get(index);
+    }
+    
+    public void addIngredient(String ingredient){
+        this.ingredients.add(ingredient);
     }
 }
