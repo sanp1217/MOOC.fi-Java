d036432caea8f90704d96d515aa602d18188f4ce
Pablo <pablosgrad12@gmail.com>
2022-05-31 11:54:55 -0600

    Makes UI class variables and constructor

---
+++ part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private Recipe recipe;
+    private Scanner scanner;
+    
+    //This class will have the interface for the user to choose commands.
+    public UserInterface(Recipe recipe, Scanner scanner){
+        this.recipe = recipe;
+        this.scanner = scanner;
+    }
+}
