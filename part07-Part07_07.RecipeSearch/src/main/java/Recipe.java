b9e56e8b785ff7d2526bfc1cc51415f1df188eb2
Pablo <pablosgrad12@gmail.com>
2022-05-30 22:17:25 -0600

    Makes recipe class with getters and setters.

--- part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
+++ part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
@@ -1,3 +1,26 @@
 public class Recipe {
+    private String name;
+    private int cookTime;
     
+    
+    public Recipe(String name, int cookTime){
+        this.name = name;
+        this.cookTime = cookTime;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public int getCookTime(){
+        return this.cookTime;
+    }
+    
+    public void setCookTime(int time){
+        this.cookTime = time;
+    }
 }
