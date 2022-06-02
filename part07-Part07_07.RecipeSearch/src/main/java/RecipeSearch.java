8049c341e4453bf53155f7d0eaca5539c1cd55f2
Pablo <pablosgrad12@gmail.com>
2022-06-01 20:58:41 -0600

    Adds method for assigning parts

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -16,15 +16,37 @@ public class RecipeSearch {
                 if(!fileLine.isEmpty()){
                     linesFromFile.add(fileLine);
                 }else{
-                    //assign recipe parts through a method.
+                    assignRecipeParts(recipes, linesFromFile);
                 }
             }
             
             if(!linesFromFile.isEmpty()){
-                //assign recipe parts through a method.
+                assignRecipeParts(recipes, linesFromFile);
             }
         } catch(Exception e){
             System.out.println("Error: " + e.getMessage() + e.getStackTrace());
         }
     }
+    
+    //To assign the name, cooking time, and ingredients to a recipe.
+    static void assignRecipeParts(ArrayList<Recipe> recipes, ArrayList<String> linesFromFile){
+        Recipe recipe = new Recipe();
+        
+        //The first element is always the name.
+        String name = linesFromFile.get(0);
+        recipe.setName(name);
+        
+        //The second is always the cook time.
+        int cookTime = Integer.valueOf(linesFromFile.get(1));
+        recipe.setCookTime(cookTime);
+        
+        //The rest of the elements are ingredients.
+        for(int i = 2; i < linesFromFile.size(); i++){
+            recipe.addIngredient(linesFromFile.get(i));
+        }
+        
+        recipes.add(recipe);
+        linesFromFile.clear();
+    }
+
 }
