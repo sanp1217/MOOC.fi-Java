0e91ef043a5c8cf07fffc11d948e6d566e664342
Pablo <pablosgrad12@gmail.com>
2022-06-06 17:33:29 -0600

    Adds addBird command

--- part07-Part07_08.BigYear/src/main/java/mainProgram.java
+++ part07-Part07_08.BigYear/src/main/java/mainProgram.java
@@ -1,13 +1,32 @@
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class mainProgram {
 
     public static void main(String[] args) {
-        // NB! Do not create other scanner objects than the one below
-        // if and when you create other classes, pass the scanner to them
-        // as a parameter
+        ArrayList<Bird> birds = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
+        
+        while(true){
+            System.out.print("? ");
+            String command = scan.nextLine();
+            
+            if(command.equals("Quit")){
+                return;
+            }else if(command.equals("Add")){
+                addBird(birds, scan);
+            }
+        }
     }
-
+    
+    public static void addBird(ArrayList<Bird> birds, Scanner scan){
+        String name, latinName;
+        System.out.print("Name: ");
+        name = scan.nextLine();
+        
+        System.out.print("Name in Latin: ");
+        latinName = scan.nextLine();
+        
+        birds.add(new Bird(name, latinName, 0));
+    }
 }
