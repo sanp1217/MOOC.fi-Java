0a0d1163b56bb0727c66f26d949a185f5fa35675
Pablo <pablosgrad12@gmail.com>
2022-06-06 17:07:13 -0600

    Adds setters and getters to Bird

--- part07-Part07_08.BigYear/src/main/java/Bird.java
+++ part07-Part07_08.BigYear/src/main/java/Bird.java
@@ -7,4 +7,28 @@ public class Bird {
         this.latinName = latinName;
         this.countObserved = countObserved;
     }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setLatinName(String name){
+        this.latinName = name;
+    }
+    
+    public String getLatinName(){
+        return this.latinName;
+    }
+    
+    public void addObservation(){
+        this.countObserved++;
+    }
+    
+    public int getObservations(){
+        return this.countObserved;
+    }
 }
